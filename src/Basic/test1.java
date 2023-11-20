20e771c43a7871b0112f07effc4caf97310190a1
shree887 <100765082+shree887@users.noreply.github.com>
2023-11-20 13:29:12 +0530

    complete project

---
+++ src/Basic/test1.java
@@ -0,0 +1,17 @@
+package Basic;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//FirefoxDriver driver = new FirefoxDriver();
+	ChromeDriver driver = new ChromeDriver();
+	//	EdgeDriver driver = new EdgeDriver();
+		//driver.get("https://www.facebook.com/");
+	}
+
+}
+			
